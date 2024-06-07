77559252e43e9b0e30f9436b0d635aba18aaf007
Kayo Souza <kayo.felipe.souza2014@gmail.com>
2024-06-07 20:25:04 -0300

    Bug fix: Improve numbers decimal precision

--- src/helpers/transformNumbers.ts
+++ src/helpers/transformNumbers.ts
@@ -1,13 +1,15 @@
 import getExponential from "./getExponential"
 
-export default function transformNumbers(a: number, b: number, exponential?: number){
+export default function transformNumbers(a: number, b: number, exponential?: number): [number, number] {
 	exponential ??= getExponential(a, b)
+
+	if(exponential === 0) return [a, b]
 
 	const [aBase, aExponential] = a.toExponential().split("e").map(Number)
 	const [bBase, bExponential] = b.toExponential().split("e").map(Number)
 
 	return [
-		aBase * 10 ** (aExponential + exponential),
-		bBase * 10 ** (bExponential + exponential)
-	] as [number, number]
+		+(aBase * 10 ** (aExponential + exponential)).toExponential(12),
+		+(bBase * 10 ** (bExponential + exponential)).toExponential(12)
+	]
 }
